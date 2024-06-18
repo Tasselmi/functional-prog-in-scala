fec889d0152bdb71c644481dc31838192b6de9b5
tasselmi <tasselmi@yeah.net>
2024-06-18 15:30:21 +0800

    using-monad

--- scala3/src/main/scala/com/rockthejvm/cats/part2abstractMath/MonadsPractice.scala
+++ scala3/src/main/scala/com/rockthejvm/cats/part2abstractMath/MonadsPractice.scala
@@ -1,5 +1,20 @@
 package com.rockthejvm.cats.part2abstractMath
 
 object MonadsPractice {
-  
+
+  import cats.Monad
+  import cats.instances.list.*
+  import cats.instances.either.*
+
+  val aManualEither: Either[String, Int] = Right(42)
+  type LoadingOr[T] = Either[String, T]
+  type ErrorOr[T]   = Either[Throwable, T]
+  val loadingMonad = Monad[LoadingOr]
+  val anEither     = loadingMonad.pure(45)
+
+  val aChangedLoading = loadingMonad.flatMap(anEither)(a =>
+    if (a % 2 == 0) Right(a + 1) else Left("loading...")
+  )
+
+  def main(args: Array[String]): Unit = {}
 }
